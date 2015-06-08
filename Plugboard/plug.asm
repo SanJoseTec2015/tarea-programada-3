d1b3f32540f0f6313f37db478298b8f571a7e164
German Vives <gvives@outlook.com>
2015-06-08 01:18:40 -0600

    PlugboardV2
    Comentado y además con el makefile correspondiente

--- Plugboard/plug.asm
+++ Plugboard/plug.asm
@@ -1,36 +1,16 @@
-%include "io.inc"
 section .bss 
     config resb 1024 ; 1 KB por Linea ; ARCHIVO DE CONFIGURACION DE LA ENIGMA
     plug resq 1;  ; Pointer to Config
 section .data
-    data db 'ESTO ES UNA PRUEBA BONITA',0
-   
-    
+    data: db 'ESTO ES UNA PRUEBA BONITA' 
+    times 1024-$+data db 0
+     
 section .text 
 
-remplazo:
-    mov rcx , 0
-    .while:
-     mov al, byte[data+rcx] 
-     inc rcx   
-     mov rbx , [plug]
-     cmp al,0
-     jz salida
-     cmp al,byte[rbx]
-     jnz remplazo.while
-     mov bl ,byte[rbx+1]
-     mov byte[data+rcx-1] , bl
-     cmp al,0
-     jnz remplazo.while  
-     
-     salida:
-         xor rax,rax 
-          
-  ret
 ;PROCEDIMIENTO PRINCIPAL DEL PLUGBOARD
-;RECIBE EL ARCHIVO DE CONFIG EN CONFIG 
-;DATA BUFFER A MODIFICAR 
-remp:
+;RECIBE EL ARCHIVO DE CONFIGURACION EN EL BUFFER CONFIG 
+;BUFFER data , texto a modificar por el PLUGBOARD
+remplazo:
    xor rax , rax
    mov [plug], rax
    mov rcx,0
@@ -48,14 +28,10 @@ remp:
        mov [plug] , rax
        mov rbx , [plug]
        mov bl ,[rbx]
-       
-     
-      
        ciclo:
-          call remplazo 
+          call remplazoaux
           mov rcx, -1
-       ciclo2:
-         
+       ciclo2:   
          inc rcx
          mov rbx , [plug] ;POS MEMORIA
          cmp byte[rbx+rcx],32 
@@ -64,16 +40,33 @@ remp:
          inc rbx 
          mov [plug] ,rbx
          cmp byte[rbx+rcx],0
-         jnz ciclo      
-       
+         jnz ciclo            
 ret
 
-global CMAIN
-CMAIN:
-    mov rbp, rsp; for correct debugging
-    ; --DEPURACION PARA SASM ---------  
-    mov rbp, rsp; for correct debugging
-    xor rax, rax
+;funcion auxiliar utilizada por remplazo 
+;remplaza el texto por el caracter
+;utiliza el puntero plug  del tamaño de palabra de 64 bits
+remplazoaux:
+    mov rcx , 0
+    .while:
+     mov al, byte[data+rcx] 
+     inc rcx   
+     mov rbx , [plug]
+     cmp al,0
+     jz salida
+     cmp al,byte[rbx]
+     jnz remplazoaux.while
+     mov bl ,byte[rbx+1]
+     mov byte[data+rcx-1] , bl
+     cmp al,0
+     jnz remplazoaux.while  
+     salida:
+         xor rax,rax 
+ ret
+
+
+global _start
+_start:
     ;-------CARGA DEL BUFFER--------------------------
     mov rdi ,0  ; STDIN
     mov rsi , config
@@ -81,14 +74,18 @@ CMAIN:
     syscall    
    ;-----------------------------------------------
        
-         call remp  ;
-         
-       ;------------IMPRIMIR 
+    call remplazo    
+
+  ;------------IMPRIMIR 
        mov rax , 1
        mov rdi,1
        mov rsi,data
-       mov rdx,1024
+       mov rdx, 1000 ; LARGO DEL BUFFER A IMPRIMIR 
        syscall
-    ret
-    
+
+       mov rax,60
+       mov rdi,0
+       syscall
+
+  
     
