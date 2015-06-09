4df573244f5c3249c91bcf7f1fbaf1bc91e63b9a
Andrés Peña <lapc20081996@gmail.com>
2015-06-08 20:36:25 -0600

    Ya logra seleccionar los rotores
    Signed-off-by: Andrés Peña <lapc20081996@gmail.com>

--- version_2_files/progra3.asm
+++ version_2_files/progra3.asm
@@ -28,16 +28,21 @@ ClearTerm: db 27,"[2J" 				; <ESC>[2J; clears display
 CLEARLEN equ $-ClearTerm 			; Length of term clear string
 
 ; EXTERN_THIS parse_files
-tabla_rotores dq varRotor1, varRotor2, varRotor3, varReflector, 0h
+lista_rotores dq varRotor1, varRotor2, varRotor3, varRotor4, varRotor5
+tabla_rotores dq 0, 0, 0, varReflector, 0h
+
+debug_qword: dq 0
 
 section .text
 global _start
 
 global MensajeAEncriptar, varMsjEncriptado, varRotor1, varRotor2, varRotor3, tabla_rotores
 global sys_write
-extern RecorrerBufferAEncriptar
+extern RecorrerBufferAEncriptar, selec_rotor, RomanosRotores
 
 _start:
+	call RomanosRotores
+	call SeleccionarRotores
 	call ClrScr
 	call RecorrerBufferAEncriptar
 	done:
@@ -60,11 +65,74 @@ ret
 sys_write:
 	push rax
 	push rdi 
-	
+	push rcx 	; IMPORTANTE: el sys_write y otros syscalls modifican rcx
+				; Experiencia personal :P
 	mov rax, 1								; sys_write (code 1)
 	mov rdi, 1								; file_descriptor (code 1 stdout)
-	syscall									
-	
+	syscall
+
+	pop rcx
 	pop rdi
 	pop rax
 ret
+
+SeleccionarRotores:
+	push r15
+	push r14
+	push r13
+	push r12
+	xor r15, r15 ; indice dentro de lista_rotores
+	xor r14, r14 ; indice dentro de tabla_rotores
+	xor r13, r13 ; indice dentro de selec_rotor
+
+	.begin:
+		lea rsi, [lista_rotores]
+		lea rdi, [tabla_rotores]
+		.ciclo:
+			xor r12, r12					; contiene la dirección a guardar en tabla_rotores
+			cmp r13, 3
+			jz .return
+			mov r15, [selec_rotor+r13*8]	; seleccione el rotor en lista_rotores
+			dec r15							; -1 porque el número empieza en 1 en el archivo
+			mov r12, [lista_rotores+r15*8]  ; *8 es la escala, son 8 bytes
+											
+			mov [tabla_rotores+r14*8], r12
+			inc r14
+			inc r13
+			jmp .ciclo
+	.return:
+
+	mov r15, [lista_rotores]
+	call debug_qword_r15
+	mov r15, [lista_rotores+8]
+	call debug_qword_r15
+	mov r15, [lista_rotores+16]
+	call debug_qword_r15
+	mov r15, [lista_rotores+24]
+	call debug_qword_r15
+	mov r15, [lista_rotores+32]
+	call debug_qword_r15
+	xor r15, r15
+	call debug_qword_r15
+	mov r15, [tabla_rotores]
+	call debug_qword_r15
+	mov r15, [tabla_rotores+8]
+	call debug_qword_r15
+	mov r15, [tabla_rotores+16]
+	call debug_qword_r15
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+	ret
+
+debug_qword_r15:
+	push rsi
+	push rdx
+	mov [debug_qword], r15
+	mov rsi, debug_qword
+	mov rdx, 8
+	call sys_write
+	pop rdx
+	pop rsi
+	ret
---
+++ version_2_files/romanosrotores.asm
@@ -0,0 +1,137 @@
+section .data
+test_buffer: db 'V,IV,IIIII,', 0x00
+selec_rotor: dq 0, 0, 0
+error_chars: db 'Se ha encontrado un caracter inválido o se ha excedido el valor requerido.', 10
+error_charsLEN: equ $-error_chars
+
+section .text
+extern sys_write
+global selec_rotor, RomanosRotores
+
+RomanosRotores:
+; El buffer debe contener solamente la línea de los rotores
+; seleccionados en el archivo.
+; Se asume que no hay otros caracteres en este buffer,
+; aparte de 'I', 'V', comas y el salto de línea.
+; Entradas:
+; rsi: buffer de entrada de los números romanos de los rotores
+; Salidas:
+; selec_rotor: se llena la tabla con los valores reales
+; Devuelve:
+; rsi: offset del último caracter, debería ser un salto de línea
+	push r14
+	push rbx
+	push rax
+	xor rsi, rsi
+	xor r14, r14	; contador comas
+	.ciclo:
+		cmp r14, 3
+		je NEAR .return
+
+		;mov r15, r14
+		;call debug_qword_r15
+		;mov r15, 0xF0
+		;call debug_qword_r15
+
+		xor rax, rax	; char actual
+		.encontrar_numero:
+			mov al, [test_buffer+rsi]
+
+			;mov r15, rax
+			;call debug_qword_r15
+			;mov r15, 0xF1
+			;call debug_qword_r15
+
+			; No incremente rsi hasta que encuentre que no hay error
+			cmp al, 0x56		; look for 'V'
+			jz .sume_cinco
+		; Se asume que no hay otros caracteres inválidos
+		; El ciclo empieza a sumar rbx=0
+			xor rbx, rbx	; numero acum
+		.sume_siguiente_char:
+			cmp rbx, 3
+			jz .siguiente_romano	; no se puede exceder de 3, III
+			mov al, [test_buffer+rsi]
+
+			;mov r15, rax
+			;call debug_qword_r15
+			;mov r15, 0xF2
+			;call debug_qword_r15
+
+			inc rsi
+			cmp al, 0x56			; encontró 'V'
+			jz .sume_tres
+			cmp al, 0x49
+			jnz .siguiente_romano	; siempre que AL sea 'I' sume uno
+
+			inc rbx					; rbx++
+
+			;mov r15, rbx
+			;call debug_qword_r15
+			;mov r15, 0xF4
+			;call debug_qword_r15
+
+			jmp .sume_siguiente_char
+
+			; Lo que sigue después de 'I' fue 'V'
+			; No puede haber leído algo como IIV ó IIIV, debe ser 'IV'
+			.sume_tres:
+				cmp rbx, 1		
+				jnz .error
+				add rbx, 3		; 1+3 = 4 jaja
+				jmp .siguiente_romano
+
+			.sume_cinco:
+				xor rbx, rbx		; borre lo que sea que tuviese acumulado
+				add rbx, 5
+				inc rsi
+
+		.siguiente_romano:
+			mov al, [test_buffer+rsi]
+
+			;mov r15, rax
+			;call debug_qword_r15
+
+			;mov r15, 0xF3
+			;call debug_qword_r15
+			
+			inc rsi						; basta para saltarse la coma del test_buffer
+
+			; Podría saltarse por ejemplo III(II) con tal de encontrar la coma
+			cmp al, 0x2C
+			jnz .siguiente_romano
+
+			;mov r15, r14
+			;call debug_qword_r15
+
+			;mov r15, 0xF5
+			;call debug_qword_r15
+
+			; guarde el número encontrado en la tabla de bytes
+			mov [selec_rotor+r14*8], rbx
+			inc r14				; siga con la siguiente coma
+
+			;mov r15, r14
+			;call debug_qword_r15
+
+			;mov r15, 0xF6
+			;call debug_qword_r15
+
+			cmp r14, 3
+			jne .ciclo			; no ha terminado la tabla
+			jmp .return			; ya llenó toda la tabla selec_rotor
+
+	.error:
+		push rdx
+		push rsi
+		mov rsi, error_chars
+		mov rdx, error_charsLEN
+		call sys_write
+		pop rsi
+		pop rdx
+
+	.return:
+	pop rax
+	pop rbx
+	pop r14
+	ret
